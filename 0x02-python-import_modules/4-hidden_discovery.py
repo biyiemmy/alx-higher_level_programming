b73aed65a7b2b3e337e9bc40d05c501c0be3cdc8
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-22 16:12:46 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
+import hidden_4 as h
 
 
-def discovr():
-    name = dir(hidden_4)
+def discover():
+    name = dir(h)
     for i in name:
         if i[:2] != '__':
             print("{:s}".format(i))
 
 
 if __name__ == "__main__":
-    discovr()
+    discover()
