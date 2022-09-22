30234cd40931351c9e7af98f1a33154b2caaca27
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-22 15:33:01 -0700

    task 2: changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
+import sys
 
 if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+    args = sys.argv
+    args.pop(0)
+    argc = len(args)
+    first_line = "{:d} argument".format(argc)
+    if argc == 0:
+        first_line += "s."
+    elif argc == 1:
+        first_line+= ":"
+    else:
+        first_line += "s:"
+    print(line1)
+    for i, _arg in enumerate(args):
+        print("{:d}: {:s}".format(i+1, _arg))
