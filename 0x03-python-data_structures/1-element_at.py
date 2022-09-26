6ba7d1ee4bbcda986ea8977249705d7699e9b3a8
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 15:53:56 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if 0 <= idx or idx >= len[my_list]:
+    """
+    gets an elment from a list at index idx
+    Args:
+        my_list - list to search
+        idx - the position to access
+    Return:
+        None - if idx is out of range
+        Data - element at idx
+    """
+
+    if 0 < idx or idx >= len(my_list):
         return my_list[idx]
     else:
         None
