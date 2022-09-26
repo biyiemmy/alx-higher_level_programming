d237a8bb1d0d06fcad3becc061ac9de2d01c0c6a
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 16:40:08 -0700

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,11 +2,11 @@ def print_reversed_list_integer(my_list=[]):
     """
     Prints integers in a list in reverse
     Args:
-        my_list - list of integers defauult []
+        my_list - list of integers default[]
     """
+
     if my_list is None:
         return None
 
     for i in reversed(my_list):
         print("{:d}".format(i))
-
