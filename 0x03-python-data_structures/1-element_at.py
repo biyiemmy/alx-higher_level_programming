5dad4b7df857d1d58b327bd37c4cd9848a347cd9
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 15:51:41 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if 0 <= idx or idx > len[my_list]:
+    if 0 <= idx or idx >= len[my_list]:
         return my_list[idx]
     else:
         None
