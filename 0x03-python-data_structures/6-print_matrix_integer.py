0439dcb531646ddf05896807248c4080f564635b
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 17:41:10 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join('{:d}'.format(j)for j in i))
+    """
+    Prints a matrix in the format
+    x x x
+    x x x
+    x x x
+    --
+    Args:
+        matrix - a matrix of integers default [[]]
+    """
+    for row in matrix:
+        fmt = " ".join(["{:d}" for x in row])
+        print(fmt.format(*row))
+
+
+if __name__ == '__main__':
+    mat_trix = [[a, a+1, a+2] for a in range(1, 16, 3)]
+    print_matrix_integer(mat_trix)
