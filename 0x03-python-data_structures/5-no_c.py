532baf1bc8c4b651fc33873410c26a19ed15e061
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 17:10:55 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,6 +9,6 @@ def no_c(my_string):
 
     new_str = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i is not "c" and i is not "C":
             new_str += i
-    return (new_str)
+    return new_str
