b7586a901dc268cf8ef6906b48b84532f39c50b2
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-26 17:25:56 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 
 def no_c(my_string):
     """
-    Returns a copy of my_str without c or C
+    Returns a copy of my_string without c or C
     Args:
-        my_str - the string to filter
+        my_string - the string to filter
     """
-
     return "".join(filter(lambda x: x not in 'cC', my_string))
