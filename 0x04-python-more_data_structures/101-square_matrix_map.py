5a4873e45684d32a22731d1cc3a9d423d448be6f
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-29 12:51:00 -0700

    task 11-

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
+#a function that computes the square value of all integers of a matrix using map
 def square_matrix_map(matrix=[]):
-    #a function that computes the square value of all integers of a matrix using map
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
