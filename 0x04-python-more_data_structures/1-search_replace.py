4b4ade19d670b3a786c634979d0f51ec573c867b
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-29 04:21:32 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,13 +2,12 @@
 
 def search_replace(my_list, search, replace):
     """
-    searxh and replace in an element in a list
+    search and replace in an element in a list
     Args: 
         my_list - the list to search
         search - element to replace
         replace - substitue for search
     """
-
     if my_list is None:
         return None
     return [replace if x == search else x for x in my_list]
