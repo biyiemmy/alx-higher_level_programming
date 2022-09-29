7a15ab9ed14172ccf2cf2f96eb2b15e8c8d84a16
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-09-29 04:18:34 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+    searxh and replace in an element in a list
+    Args: 
+        my_list - the list to search
+        search - element to replace
+        replace - substitue for search
+    """
+
+    if my_list is None:
+        return None
+    return [replace if x == search else x for x in my_list]
