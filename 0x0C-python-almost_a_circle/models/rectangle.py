9fe58305a762e113710ee8fe63e5c98ced94a326
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2022-10-30 12:24:08 -0700

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Class Rectangle '''
+
+from models.base import Base
+
+    
+class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
