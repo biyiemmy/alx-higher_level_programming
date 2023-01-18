b158fd7dcff21d230206db0823186089f639b019
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2023-01-18 05:08:13 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""this script displays the X-Request-Id header variable of a request to a given URL
+"""
+this script displays the X-Request-Id header 
+variable of a request to a given URL
 """
 import sys
 import requests
