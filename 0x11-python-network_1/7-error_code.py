8503553ff0391c51bfb0684e0121ad2ba53f5acc
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2023-01-18 05:16:44 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
-this script takes in a URL and an email address, sends a POST request to the
-passed URL with the email as a parameter, and finally displays the body
-of the response.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
+import sys
 import requests
-from sys import argv
 
 
 if __name__ == "__main__":
